a9a6c91067ab7dd9d12bd0ebf208223baeb1fdae
lama mahmoud <lamamahmoud213@gmail.com>
2022-05-22 04:22:59 +0300

    playing sound on new level start

--- game.js
+++ game.js
@@ -8,21 +8,34 @@ window.onload= function(){
 	// initiate the game
 	document.onkeypress = function (){
 		var rand=rand();
+		
+		colorsArrangement.push(rand);
+		
+		
 		if(rand===0)
 		{
-			
+			let green=document.getElementById("green");
+			var audio = new Audio('sounds/green.mp3');
+			audio.play();
 		}
 		if(rand===1)
 		{
+			let red=document.getElementById("red");
+			var audio = new Audio('sounds/red.mp3');
+			audio.play();
 			
 		}
 		if(rand===2)
 		{
-			
+			let yellow=document.getElementById("yellow");
+			var audio = new Audio('sounds/yellow.mp3');
+			audio.play();
 		}
 		if(rand===3)
 		{
-			
+			let blue=document.getElementById("blue");
+			var audio = new Audio('sounds/blue.mp3');
+			audio.play();
 		}
 		
 	}
