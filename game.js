6e52d5c23ba657a50941dfb50b51e2cc5b97cff8
lama mahmoud <lamamahmoud213@gmail.com>
2022-05-22 04:07:55 +0300

    Initiating game

---
+++ game.js
@@ -0,0 +1,29 @@
+window.onload= function(){
+	
+	let colorsArrangement=[];
+	
+	function rand(){
+		return Math.floor(Math.random()*4));
+	}
+	// initiate the game
+	document.onkeypress = function (){
+		var rand=rand();
+		if(rand===0)
+		{
+			
+		}
+		if(rand===1)
+		{
+			
+		}
+		if(rand===2)
+		{
+			
+		}
+		if(rand===3)
+		{
+			
+		}
+		
+	}
+}
